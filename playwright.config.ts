6b5cbce3a78464501ef3ad1c94cbafb5e14e8e6b
raeperd <raeperd117@gmail.com>
2026-09-21 00:09:26 +0900

    test: use half of local CPU cores for workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
-  workers: process.env.CI ? 3 : 1,
+  workers: process.env.CI ? 3 : "50%",
   use: { actionTimeout: 5000 },
   timeout: 30_000,
   reporter: "list",
